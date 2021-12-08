c31c24ce830808fcc3d3c9665de09df290d9cc03
BekhAndrey <andreyac17@gmail.com>
2021-12-08 18:22:33 +0300

    Added custom .js file

---
+++ src/main/webapp/static/js/functions.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    var enter = new Date();
+    enter.setDate(enter.getDate() + 1);
+    $(".enterdatepicker").datepicker({
+        todayBtn:  1,
+        autoclose: true,
+        format: "dd/mm/yyyy",
+        startDate: enter
+    }).on('changeDate', function (selected) {
+        var minDate = new Date(selected.date.valueOf());
+        minDate.setDate(minDate.getDate()+1);
+        $('.exitdatepicker').datepicker('setStartDate', minDate);
+    });
+
+    var exit = new Date();
+    exit.setDate(exit.getDate() + 2);
+    $(".exitdatepicker").datepicker({
+        autoclose: true,
+        format: "dd/mm/yyyy",
+        startDate: exit
+    }).on('changeDate', function (selected) {
+        var maxDate = new Date(selected.date.valueOf());
+        $('.enterdatepicker').datepicker('setEndDate', maxDate);
+    });
+
+    $('#btn_modal').click(function () {
+
+        myModal = new bootstrap.Modal(document.getElementById('modal'), {
+            keyboard: false
+        })
+        myModal.show()
+
+    });
+});
+
+
